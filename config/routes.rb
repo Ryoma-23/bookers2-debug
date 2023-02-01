118e4c1e404be45ef4723f892b346246217d744c
Ryoma-23 <120545081+Ryoma-23@users.noreply.github.com>
2023-02-01 07:37:52 +0000

    [Add] グループへの参加、退会　メンバー表示

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,8 @@ Rails.application.routes.draw do
     get 'followers' => 'relationships#followers', as: 'followers'
   end
   resources :chats, only: [:show, :create]
-  resources :groups, except: [:destroy]
+  resources :groups do
+    get "join" => "groups#join"
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
