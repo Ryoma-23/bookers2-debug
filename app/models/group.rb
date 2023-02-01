c78d2de497d2c00db09f8726a87e683ca13eb67e
Ryoma-23 <120545081+Ryoma-23@users.noreply.github.com>
2023-02-01 06:54:56 +0000

    [Add] グループ作成

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,9 +1,12 @@
 class Group < ApplicationRecord
   has_many :group_users
   has_many :users, through: :group_users
-  has_one_attached :image
+  has_one_attached :profile_image
 
   validates :name, presence: true
   validates :introduction, presence: true
-  attachment :image, destroy: false
+  #attachment :image, destroy: false
+  def get_profile_image
+    (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
 end
